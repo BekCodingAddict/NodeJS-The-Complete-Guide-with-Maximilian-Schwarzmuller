77d79dca711816c9a5619cee051eaa8f7088bac5
unknown <experte001@gmail.com>
2025-02-07 16:48:41 +0900

    Saving the data to mongoose has done

--- 13.Mongoose/models/product.js
+++ 13.Mongoose/models/product.js
@@ -15,12 +15,13 @@ const productSchema = new Schema({
     type: String,
     required: true,
   },
-  imageURL: {
+  imageUrl: {
     type: String,
     required: true,
   },
 });
 
+module.exports = mongoose.model("Product", productSchema);
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 
