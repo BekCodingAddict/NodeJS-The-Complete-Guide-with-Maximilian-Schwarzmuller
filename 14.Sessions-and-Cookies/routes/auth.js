7bb979d82a394ca01ab2958af58e09192420c174
unknown <experte001@gmail.com>
2025-02-21 08:43:46 +0900

    #Sessions: Adding Request Driven Login Soluiton

--- 14.Sessions-and-Cookies/routes/auth.js
+++ 14.Sessions-and-Cookies/routes/auth.js
@@ -1,8 +1,9 @@
 const path = require("path");
-
 const express = require("express");
 const router = require("./admin");
 const authController = require("../controllers/auth");
+
 router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
 
 module.exports = router;
