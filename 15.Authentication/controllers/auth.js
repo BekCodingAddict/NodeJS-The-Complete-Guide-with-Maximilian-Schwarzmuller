cefdfaa5c750fb8d12c469a881ad544f54e53d1f
unknown <experte001@gmail.com>
2025-03-02 13:20:48 +0900

    #Authentication: Adding the Signin Functionality🔨

--- 15.Authentication/controllers/auth.js
+++ 15.Authentication/controllers/auth.js
@@ -24,32 +24,50 @@ exports.postSignUp = (req, res, next) => {
       if (userDoc) {
         return res.redirect("/signup");
       }
-      return bcrypt.hash(password, 12);
-    })
-    .then((hashedPassword) => {
-      const user = new User({
-        email: email,
-        password: hashedPassword,
-        cart: { items: [] },
-      });
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
 
-      return user.save();
-    })
-    .then((result) => {
-      res.redirect("/login");
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/login");
+        });
     })
     .catch((error) => console.log(error));
 };
 
 exports.postLogin = async (req, res, next) => {
-  await User.findById("67b4cef85b426d77532bb2d0")
+  const email = req.body.email;
+  const password = req.body.password;
+
+  await User.findOne({ email: email })
     .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((error) => {
-        console.log(error);
-        res.redirect("/");
-      });
+      if (!user) {
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((error) => {
+              console.log(error);
+              res.redirect("/");
+            });
+          }
+          res.redirect("/login");
+        })
+        .catch((error) => {
+          console.log(error);
+          res.redirect("/login");
+        });
     })
     .catch((error) => console.log(error));
 };
