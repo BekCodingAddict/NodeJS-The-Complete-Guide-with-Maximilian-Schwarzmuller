ed41a6e8494afadb7aad52adf50372f410aed022
unknown <experte001@gmail.com>
2025-03-02 12:36:47 +0900

    #Authentication: Sign Up Views SetUps🎯

---
+++ 15.Authentication/app.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+const MongoDBStore = require("connect-mongodb-session")(session);
+
+const app = express();
+const store = new MongoDBStore({
+  uri: "mongodb+srv://bekcodingaddict:Otabek97@expressbus.lorhvlo.mongodb.net/Test",
+  collection: "sessions", // Rename it to "sessions" (standard convention)
+  expires: 1000 * 60 * 60 * 24, // 1 day in milliseconds
+});
+
+store.on("error", function (error) {
+  console.log(error);
+});
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(async (req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  await User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((error) => console.log(error));
+});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    "mongodb+srv://bekcodingaddict:Otabek97@expressbus.lorhvlo.mongodb.net/Test",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(async () => {
+    let user = await User.findOne();
+    if (!user) {
+      user = new User({
+        name: "John",
+        email: "John@text.com",
+        cart: { items: [] },
+      });
+      await user.save();
+    }
+    console.log("Connected to MongoDB ✅");
+    app.listen(3000, () => console.log("Server is running on port 3000 🚀"));
+  })
+  .catch((err) => console.log("MongoDB connection error:", err));
---
+++ 15.Authentication/controllers/auth.js
@@ -0,0 +1,35 @@
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
+  });
+};
+exports.getSignUp = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
+  });
+};
+
+exports.postLogin = async (req, res, next) => {
+  await User.findById("67b4cef85b426d77532bb2d0")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((error) => {
+        console.log(error);
+        res.redirect("/");
+      });
+    })
+    .catch((error) => console.log(error));
+};
+exports.postLogout = async (req, res, next) => {
+  req.session.destroy((error) => {
+    console.log(error);
+    res.redirect("/");
+  });
+};
