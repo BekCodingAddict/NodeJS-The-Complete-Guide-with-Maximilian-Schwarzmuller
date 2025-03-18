7aa7aacb76689d510e7a65520f3dd0aa6f68eba4
unknown <experte001@gmail.com>
2025-03-19 05:16:36 +0900

    #Validation: Adding to Login Validation ✅

--- 18.Understanding-Validation/routes/auth.js
+++ 18.Understanding-Validation/routes/auth.js
@@ -10,25 +10,8 @@ router.get("/login", authController.getLogin);
 router.post(
   "/login",
   [
-    check("email")
-      .isEmail()
-      .withMessage("Please enter a valid email!")
-      .custom((value, { req }) => {
-        // if (value === "test@test.com") {
-        //   throw new Error("This email address if forbitten");
-        // }
-        //return true;
-
-        return User.findOne({ email: value }).then((userDoc) => {
-          if (userDoc) {
-            return Promise.reject("E-mail already exist!");
-          }
-        });
-      }),
-    body(
-      "password",
-      '"Please enter a password with only numbers and text and at least 5 characters"'
-    )
+    body("email").isEmail().withMessage("Please enter a valid email address."),
+    body("password", "Password has to be valid.")
       .isLength({ min: 5 })
       .isAlphanumeric(),
   ],
