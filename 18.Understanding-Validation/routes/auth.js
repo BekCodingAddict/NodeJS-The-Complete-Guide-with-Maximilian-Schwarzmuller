ff31f96af57800717d4803c644ac58e91b9df76e
unknown <experte001@gmail.com>
2025-03-19 05:53:00 +0900

    #Validation: Sanitizing Data ⚙

--- 18.Understanding-Validation/routes/auth.js
+++ 18.Understanding-Validation/routes/auth.js
@@ -13,7 +13,8 @@ router.post(
     body("email").isEmail().withMessage("Please enter a valid email address."),
     body("password", "Password has to be valid.")
       .isLength({ min: 5 })
-      .isAlphanumeric(),
+      .isAlphanumeric()
+      .trim(),
   ],
   authController.postLogin
 );
@@ -35,19 +36,23 @@ router.post(
             return Promise.reject("E-mail already exist!");
           }
         });
-      }),
+      })
+      .normalizeEmail(),
     body(
       "password",
       '"Please enter a password with only numbers and text and at least 5 characters"'
     )
       .isLength({ min: 5 })
-      .isAlphanumeric(),
-    body("confirmPassword").custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error("Password have to match!");
-      }
-      return true;
-    }),
+      .isAlphanumeric()
+      .trim(),
+    body("confirmPassword")
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Password have to match!");
+        }
+        return true;
+      }),
   ],
   authController.postSignUp
 );
