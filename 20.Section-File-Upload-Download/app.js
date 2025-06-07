be4ae36f57ae497435ab4a8d0eda05414ec35111
unknown <experte001@gmail.com>
2025-06-08 00:27:00 +0900

    Initial Commits

---
+++ 20.Section-File-Upload-Download/app.js
@@ -0,0 +1,85 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+require("dotenv").config();
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+const app = express();
+const store = new MongoDBStore({
+	uri: process.env.MONGODB_URL,
+	collection: "sessions",
+});
+const csrfProtection = csrf();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+	session({
+		secret: "my secret",
+		resave: false,
+		saveUninitialized: false,
+		store: store,
+	})
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+	if (!req.session.user) {
+		return next();
+	}
+	User.findById(req.session.user._id)
+		.then((user) => {
+			if (!user) {
+				return next();
+			}
+			req.user = user;
+			next();
+		})
+		.catch((err) => {
+			next(new Error(err));
+		});
+});
+
+app.use((req, res, next) => {
+	res.locals.isAuthenticated = req.session.isLoggedIn;
+	res.locals.csrfToken = req.csrfToken();
+	next();
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.get("/500", errorController.get500);
+
+app.use(errorController.get404);
+app.use((error, req, res, next) => {
+	res.riderect("/500 ");
+});
+
+mongoose
+	.connect(process.env.MONGODB_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(async () => {
+		console.log("Connected to MongoDB ✅");
+		app.listen(3000, () => console.log("Server is running on port 3000 🚀"));
+	})
+	.catch((err) => console.log("MongoDB connection error:", err));
